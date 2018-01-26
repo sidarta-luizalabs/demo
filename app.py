365ae8eaac34e82335bfc872285ed2c3dede1fa3
sidarta <sidarta@luizalabs.com>
2018-01-26 01:11:14 -0200

    demo: testing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def slash():
-  return "hostname-%s" % socket.gethostname()
+  return "%s - luizalabs" % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
