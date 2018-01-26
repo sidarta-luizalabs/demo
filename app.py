74fd67ea13b5f61f91180c96ef9a7c6bc67a13bb
sidarta <sidarta@luizalabs.com>
2018-01-26 17:38:51 -0200

    demo: jaja

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def slash():
-  return "%s - xpto" % socket.gethostname()
+  return "%s - Jaja e o cara" % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
