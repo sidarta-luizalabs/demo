0888bfd2c22e25a1d4fb83ac2918ddd7e72196ff
sidarta <sidarta@luizalabs.com>
2018-01-26 10:59:20 -0200

    demo: xpto

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def slash():
-  return "%s - luizalabs" % socket.gethostname()
+  return "%s - xpto" % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
